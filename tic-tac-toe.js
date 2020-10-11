eefc41721882f778e0098226a8d8b42cce45681f
Lilly Vernon <littletomoya@yahoo.com>
2020-10-11 15:10:09 -0500

    check for the winner and update status as well as disallow cheating

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,13 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() { mainfunction() });
+
+function mainfunction() {
 
     let addsquare = document.getElementById("board").children;
     var player_one = 1;
+    var player1arr = [];
+    var player2arr = [];
 
     for (let i = 0; i < addsquare.length; i++) {
         addsquare[i].classList.add("square")
         addsquare[i].onclick = function() { myFunction() };
-        var player1arr = [];
-        var player2arr = [];
         addsquare[i].addEventListener('mouseover',
             function(e) { e.target.classList.add('hover') });
 
@@ -15,12 +17,11 @@ document.addEventListener("DOMContentLoaded", function() {
             function(e) { e.target.classList.remove('hover') });
 
 
-
         function myFunction() {
 
-            ranNum = Math.floor(Math.random() * 2) + 1;
 
-            clicked = false;
+
+
             if (player_one == 1) {
 
                 addsquare[i].innerHTML = "X";
@@ -28,6 +29,14 @@ document.addEventListener("DOMContentLoaded", function() {
                 player_one = 0;
                 player1arr.push(i);
                 addsquare[i].onclick = false;
+                if (player1arr.includes(0) && player1arr.includes(1) && player1arr.includes(2) ||
+                    player1arr.includes(3) && player1arr.includes(4) && player1arr.includes(5) ||
+                    player1arr.includes(6) && player1arr.includes(7) && player1arr.includes(8)) {
+                    document.getElementById("status").classList.add("you-won")
+                    document.getElementById("status").innerHTML = "Congratulations! X is the Winner!"
+                }
+
+
 
             } else {
 
@@ -37,19 +46,30 @@ document.addEventListener("DOMContentLoaded", function() {
                 player2arr.push(i);
                 addsquare[i].onclick = false;
 
+                if (player2arr.includes(0) && player2arr.includes(1) && player2arr.includes(2) ||
+                    player2arr.includes(3) && player2arr.includes(4) && player2arr.includes(5) ||
+                    player2arr.includes(6) && player2arr.includes(7) && player2arr.includes(8)) {
+                    document.getElementById("status").classList.add("you-won")
+                    document.getElementById("status").innerHTML = "Congratulations! O is the Winner!"
+                }
+
             }
         }
+
         const restartGame = document.getElementsByClassName("btn")
         restartGame[0].addEventListener('click', function() {
             addsquare[i].innerHTML = ""
-            alert("New Game")
+            mainfunction();
+
         });
 
 
     }
+
     console.log(player1arr)
     console.log(player2arr)
-
-
-
-});
+        /*if ((player1arr.includes("0") && player1arr.includes("1") && player1arr.includes("2")) ||
+            (player1arr.includes("3") && player1arr.includes("4") && player1arr.includes("5")) ||
+            (player1arr.includes("6") && player1arr.includes("7") && player1arr.includes("8"))) {
+            alert("Player 1 is the winner");*/
+}
